2b86abc2875c64dfb86110c02325bbbb51e4997f
mur1chan <126001665+mur1chan@users.noreply.github.com>
2024-06-03 06:13:31 +0200

    Revert "clean up"
    This reverts commit fd48fb01d3ab02416abcff9d480faf58a383471f.

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { memo } from 'react';
+import type { FC } from 'react';
+
+import classes from './App.module.css';
+import resets from './components/_resets.module.css';
+import { Desktop1 } from './components/Desktop1/Desktop1.js';
+
+interface Props {
+  className?: string;
+}
+export const App: FC<Props> = memo(function App(props = {}) {
+  return (
+    <div className={`${resets.clapyResets} ${classes.root}`}>
+      <Desktop1 />
+    </div>
+  );
+});
